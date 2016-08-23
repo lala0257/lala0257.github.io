4778aff9851c34bc11a945c62663ad4047c065d2
lala0257 <chenyu0257@163.com>
2016-08-23 11:27:17 +0800

    advertise part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 /**
  * Created by Administrator on 2016/8/22.
  */
+'use strict';
+$(function(){
+    function resize(){
+        var windowWidth = $(window).width();
+        var isSmallScreen = windowWidth<768;
+        $('#carousel-example-generic .carousel-inner .item').each(function(k,v){
+            var data_src="";
+            if(isSmallScreen){
+                data_src = $(v).find('img').attr('data-image-xs');
+            }else{
+                data_src = $(v).find('img').attr('data-image-lg');
+            }
+            $(v).find('img').attr("src",data_src);
+        });
+    }
+    $(window).on('resize',resize).trigger('resize');
+});
