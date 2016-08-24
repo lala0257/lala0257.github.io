33da652547242a5ed0edb7cf7b7fc99d416eb007
lala0257 <chenyu0257@163.com>
2016-08-24 11:16:38 +0800

    scroll

--- js/main.js
+++ js/main.js
@@ -17,4 +17,23 @@ $(function(){
         });
     }
     $(window).on('resize',resize).trigger('resize');
+
+    //初始tooltips插件
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //控制标签容器宽度
+
+    var ulWapperContent = $('.nav-tabs');
+    var width = 0;
+    ulWapperContent.children().each(function(index,element){
+        width += $(element).width();
+    });
+
+    //判断当前ul的宽是否超出屏幕
+    if(width>$(window).width()){
+        ulWapperContent.css("width",width+20);
+    }
+
 });
+
+
